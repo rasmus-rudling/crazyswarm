7fa24fe7dac8f492e98d5ebb6d0de4749fba2df1
Rasmus Rudling <rasmusrudling@gmail.com>
2022-06-20 08:42:44 +0200

    Participant #2

--- ros_ws/src/crazyswarm/scripts/perceived-safety-study/shellScripts/plotSF2.sh
+++ ros_ws/src/crazyswarm/scripts/perceived-safety-study/shellScripts/plotSF2.sh
@@ -1,7 +1,7 @@
 #! bin/bash
 
-# cd /home/rpl/Documents/rasmus/crazyswarm/ros_ws/src/crazyswarm/scripts/perceived-safety-study/preStudy
-cd /Users/rr/Documents/thesis/degree-thesis/ros_ws/src/crazyswarm/scripts/perceived-safety-study/preStudy
+cd /home/rpl/Documents/rasmus/crazyswarm/ros_ws/src/crazyswarm/scripts/perceived-safety-study/preStudy
+# cd /Users/rr/Documents/thesis/degree-thesis/ros_ws/src/crazyswarm/scripts/perceived-safety-study/preStudy
 
 echo "Plotting SF2"
 python3 main.py plot
